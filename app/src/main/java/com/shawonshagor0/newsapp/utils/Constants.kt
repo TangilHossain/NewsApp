8d61e9c042088170a312da17c4424d5be5bafd5a
Tangil Hossain Shawon <70896114+TangilHossain@users.noreply.github.com>
2025-03-23 02:00:05 +0600

    Update Constants.kt
    Hidden API Key

--- app/src/main/java/com/shawonshagor0/newsapp/utils/Constants.kt
+++ app/src/main/java/com/shawonshagor0/newsapp/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.shawonshagor0.newsapp.utils
 
 class Constants {
     companion object{
-        const val API_KEY = "53e0bc01d0e64603ae0b8b75d7c85517"
+        const val API_KEY = "YOUR_API_KEY"
         const val BASE_URL = "https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
